ae133e7d4bb3d68d64e246969633a574b52f8f79
hulrog <petar99t@gmail.com>
2022-08-30 10:44:52 +0200

    added classes for person and house

---
+++ model/house.php
@@ -0,0 +1,44 @@
+<?php
+class House{
+    public $houseID;
+    public $name;
+    public $seat;
+    public $region;
+
+    public function __construct($houseID=null, $name=null, $seat=null, $region=null){
+        $this->houseID = $houseID;
+        $this->name = $name;
+        $this->seat = $seat;
+        $this->region = $region;
+
+    }
+
+    public static function getAll(mysqli $conn){
+
+        $sql = "SELECT * FROM houses";
+        return $conn->query($sql);
+
+    }
+
+    
+    public static function deleteById($houseID, mysqli $conn){
+
+        $sql = "DELETE FROM `houses` WHERE `houseID`=$houseID";
+        return $conn->query($sql);
+
+    }
+
+   public static function add(House $house, mysqli $conn){
+    $seat = mysqli_real_escape_string($conn, $house->seat);//for the apostrophes King's Landing, Storm's End etc.
+
+    $sql = "INSERT INTO `houses` (`name`,`seat`,`region`) 
+    VALUES('$house->name', '$seat', '$house->region')";
+   
+    return $conn->query($sql);
+
+    }
+
+
+}
+
+?>
